9a2fc33034c12280ab48621cd5f62e964a44a46d
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-08-15 15:22:51 +0100

    Add data cleaning function

--- unicity/clean.py
+++ unicity/clean.py
@@ -3,3 +3,8 @@ import pandas as pd
 def date_resolution(start: pd.DataFrame, end: pd.DataFrame, resolution: str):
     time = (end - start) / 2 + start
     return time.apply(lambda x: pd.Timestamp(x).round(resolution))
+
+def clean_data(data: pd.DataFrame):
+    data["time"] = date_resolution(data["start time"], data["end time"], "60min")
+    data = data.drop(["start time", "end time", "month", "date"], axis=1)
+    return data.drop_duplicates(subset=["user id", "time"])
