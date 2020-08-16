21effe62bd8d3ff4d9128038ad05988c4e56937b
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-08-16 17:14:25 +0100

    Add plot for unicity against p

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -1,7 +1,17 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 def main():
     data = pd.read_csv("results/2014-06-01.csv")
     print(data)
+    plt.figure()
+    a = plt.axes()
+    a.set_xlabel("Data points revealed")
+    a.set_ylabel("Unicity")
+    a.set_title("Unicity against p")
+    for res in set(data["resolution"]):
+        d = data[data["resolution"] == res]
+        plt.plot(d["p"], d["unicity"])
+    plt.show()
 
 main()
