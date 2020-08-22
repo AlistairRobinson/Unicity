68969cbab7c4187e8aabe40d42c650edb67506bf
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-08-22 15:31:48 +0100

    Modify default result file

--- unicity/parser.py
+++ unicity/parser.py
@@ -7,7 +7,7 @@ def construct_parser() -> argparse.PARSER:
     parser.add_argument("-p", help="the number of data points exposed to calculate unicity",
         default=1, type=int)
     parser.add_argument("-f", help="the .csv file to read data from",
-        default="data/telecom_data.csv")
+        default="data/60min.csv")
     parser.add_argument("-u", help="the user id column of the data",
         default="user id")
     parser.add_argument("-s", help="the date to start observations from",
